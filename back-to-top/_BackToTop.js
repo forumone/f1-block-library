41c0dbfb3b052492acc3a592bd5dff821d92caef
KJ Monahan <kj@forumone.com>
2021-12-14 17:10:13 -0600

    Add and configure linting

--- back-to-top/_BackToTop.js
+++ back-to-top/_BackToTop.js
@@ -1,16 +1,16 @@
-import {__} from '@wordpress/i18n';
+import { __ } from '@wordpress/i18n';
 import Arrow from './_Arrow';
 
 function BackToTop({
-  topElement = 'top',
-  title = __('Back to Top'),
-  ...attributes
-                   }) {
-  return (
-    <a href={`#${topElement}`} title={title} {...attributes}>
-      <Arrow className="back-to-top__icon" />
-    </a>
-  )
+	topElement = 'top',
+	title = __('Back to Top'),
+	...attributes
+}) {
+	return (
+		<a href={`#${topElement}`} title={title} {...attributes}>
+			<Arrow className="back-to-top__icon" />
+		</a>
+	);
 }
 
 export default BackToTop;
--- back-to-top/_Save.js
+++ back-to-top/_Save.js
@@ -1,17 +1,18 @@
-import {useBlockProps} from '@wordpress/block-editor';
+import { useBlockProps } from '@wordpress/block-editor';
 import BackToTop from './_BackToTop';
 import classnames from 'classnames';
 
-function Save( { attributes } ) {
-  const { topElement, title } = attributes;
-  const blockProps = useBlockProps.save();
-  return (
-    <BackToTop
-      {...blockProps}
-      className={classnames(blockProps.className, 'back-to-top')}
-      topElement={topElement}
-      title={title}
-    />);
+function Save({ attributes }) {
+	const { topElement, title } = attributes;
+	const blockProps = useBlockProps.save();
+	return (
+		<BackToTop
+			{...blockProps}
+			className={classnames(blockProps.className, 'back-to-top')}
+			topElement={topElement}
+			title={title}
+		/>
+	);
 }
 
 export default Save;

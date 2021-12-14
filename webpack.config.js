7bc45858fd7cb82d2e1d758cb1fbc35d2888dc9c
KJ Monahan <kj@forumone.com>
2021-12-14 10:03:09 -0600

    Set up block plugin build scripts

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const defaultConfig = require('@wordpress/scripts/config/webpack.config');
+const path = require('path');
+const glob = require('glob');
+module.exports = {
+  ...defaultConfig,
+  entry: () => {
+    return glob
+      .sync('!(build|node_modules)/**/*.js', {
+        ignore: ['**/_*'],
+      })
+      .reduce((entries, currentFile) => {
+        const updatedEntries = entries;
+        const [newFilePath] = path.dirname(currentFile).split(path.sep).slice(-1);
+        console.log()
+        // Throw an error if duplicate files detected.
+        if (updatedEntries[newFilePath]) {
+          throw new Error(`More than one file named ${newFilePath}.js found.`);
+        }
+        updatedEntries[newFilePath] = {
+          import: path.resolve(__dirname, currentFile),
+        };
+        return updatedEntries;
+      }, {});
+  }
+}
